5b6e166c59d0863428c193eb5dd96d1701c4de27
Aiden Ko <aidensko@gmail.com>
2025-02-02 15:59:54 +0900

    Changes to be committed: 	modified:   emojiMoods.py 	modified:   static/css/about.css 	modified:   static/css/index.css 	deleted:    templates/about copy.html 	modified:   templates/about.html 	deleted:    templates/index copy.html 	modified:   templates/index.html

--- emojiMoods.py
+++ emojiMoods.py
@@ -13,9 +13,9 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 def interpretEmojis(user_input):
     try:
         prompt = f"""
-        Given this emoji input: {user_input}, generate three advanced, difficult, obscure English words and three advanced Korean words, but all of them with English definitions.
+        Given this emoji input: {user_input}, generate three advanced, difficult, obscure English words (words that are beyond a college student's level) and three advanced Korean words, but all of them with English definitions.
         These words should be those that best encapsulate ALL of the emojis, as the emojis have been picked in that combination to try to describe a certain mood.
-        Format each as 'Word - Definition'.
+        Format each as 'Word - Definition'. Keep the definitions fairly short.
         Do not add any extra content to your response outside of the six words and their english definitions. Each word-definition pair should be on their own line.
         """
 
